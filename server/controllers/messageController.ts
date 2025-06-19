6cdd34a656027851e302cae4ee9e6780fe64bcd0
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-06-19 15:41:22 -0500

    feat (db controllers): add template logic for user, room, message, and auth

--- server/controllers/messageController.ts
+++ server/controllers/messageController.ts
@@ -1,2 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+/** 
+ * input: userId, roomId, content (text or emoji or image)
+ * return: messageID
+ */
+const createMessage = () => {}
+
+/** 
+ * input: messageID
+ * return: 200 or fail
+ */
+const deleteMessage = () => {}
--- server/controllers/roomController.ts
+++ server/controllers/roomController.ts
@@ -1,2 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+/**
+ * Return array of all active private rooms. each room has id, name, user count
+ * OR
+ * Return array of all active public rooms. each room has id, name,  user count, password prompt
+ */
+export const getRooms = () => {};
+
+/** */
+export const createRoom = () => {};
