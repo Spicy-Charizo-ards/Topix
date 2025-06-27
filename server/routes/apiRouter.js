806d431c0d7526ad3e0b224631df2e74895712bf
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-06-26 19:52:36 -0500

    feat (api): add endpoints for room requests
    - convert controller functions to middleware syntax

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,12 +1,30 @@
 import express from 'express';
+import { getRooms, createRoom } from '../controllers/roomController.ts';
 import { getRoomMesages } from '../controllers/messageController.ts';
 
 const apiRouter = express.Router();
 
 // get request that (endpoint, controller)
-apiRouter.get('/getMessages', getRoomMesages, (req,res) => {
-    return res.status(200).json(res.locals.messages);
+
+
+// GET PUBLIC ROOMS - RETURN LIST OF ALL ROOMS
+apiRouter.get('/rooms', getRooms, (req,res) => {
+    return res.status(200).json({rooms: res.locals.rooms});
 });
+
+// CREATE A ROOM
+apiRouter.post('/rooms', createRoom, (req, res)=> {
+    return res.status(201).json(room: {res.locals.room});
+
+});
+
+// GET ROOM DETAILS AND  MESSAGES 
+apiRouter.get('/rooms/:roomId', getRoomMesages, (req,res) => {
+    return res.status(200).json({room: res.locals.roomMessages});
+});
+
+
+
 
 
 export default apiRouter;
